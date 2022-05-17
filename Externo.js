0514ce11179a2cdc35ed9327844b9ae15a93256c
studTon <evertonsdasilva1@gmail.com>
2022-05-17 00:26:33 -0300

    Exercício do JS externo

---
+++ Externo.js
@@ -0,0 +1,17 @@
+let messagem = window.document.getElementById('resultado');
+
+function acao1() {
+    messagem.innerHTML += '<p>Clicou no botão 1</p>';
+}
+
+function acao2() {
+    messagem.innerHTML += '<p>Clicou no botão 2</p>';
+}
+
+function acao3() {
+    messagem.innerHTML += '<p>Clicou no botão 3</p>';
+}
+
+function acao4() {
+    messagem.innerHTML += '<p>Clicou no botão 4</p>';
+}
